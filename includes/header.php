8d30ff286bdea4de5b1e2181cf3c64af5726c9d1
Wan arif <wanjalalularif123@gmail.com>
2026-08-27 06:24:52 +0000

    header.php

--- includes/header.php
+++ includes/header.php
@@ -3,9 +3,13 @@
       <h1><a href="index.html" class="brand">Course Registration System</a></h1>
       <nav>
         <a href="index.html">Home</a>
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
+        <?php if (isset($_SESSION['user_id'])): ?>
+          <a href="dashboard.php">Dashboard</a>
+          <a href="logout.php">Logout</a>
+        <?php else: ?>
+          <a href="login.php">Login</a>
+          <a href="register.php">Register</a>
+        <?php endif; ?>
       </nav>
     </div>
   </header>
- 
